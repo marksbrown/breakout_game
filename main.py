6037bae3c1829d76cc8013ccbb499c7a86b27bc7
Mark Brown (MSB) <MBrown@StGeorgesWeybridge.com>
2019-11-12 14:58:56 +0000

    added paddle

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pyxel
+from paddle import Paddle
 
 
 class App:
+    screen_size = (160, 120)
+
+    @property
+    def objects(self):
+        yield self.paddle
+
     def __init__(self):
-        pyxel.init(160, 120)
-        self.x = 0
+        pyxel.init(*App.screen_size)
+        self.paddle = Paddle(App.screen_size)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        
+        for obj in self.objects:
+            obj.update()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        for obj in self.objects:
+            obj.draw()
+        
 
 
 if __name__ == "__main__":
--- paddle.py
+++ paddle.py
@@ -1,16 +1,69 @@
 import pyxel
+from abc import ABC, abstractmethod
 
-class Rectangle:
+
+class Rectangle(ABC):
     """
     Draw rectangle at x,y with width and height
+    sx, sy : screen width and height
     """
+    @property
+    def left(self):
+        return self.x - self.width // 2
+
+    @property
+    def right(self):
+        return self.x + self.width // 2
+
+    @property
+    def top(self):
+        return self.y - self.height // 2
+
+    @property
+    def bottom(self):
+        return self.y + self.height // 2
+
+    def __init__(self, x, y, width, height, sx, sy, colour):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.sx = sx
+        self.sy = sy
+        self.colour = colour
+
+    def draw(self):
+        pyxel.rect(self.left, self.top, self.width, self.height, self.colour)
+
+    @abstractmethod
+    def update(self):
+        pass
 
 
-class Paddle:
+class Paddle(Rectangle):
     colour = 1
-    width = 50
-    height = 10
+    width = 20
+    height = 4
+    margin = 1  # shift if we get stuck
+    keys = {pyxel.KEY_A: -2,
+            pyxel.KEY_D: 2}
 
     def __init__(self, screen_size):
-        self.sx, self.sy = screen_size
-        self.x = self.sx // 2
+        sx, sy = screen_size
+        super().__init__(sx // 2, int(sy*0.9), 
+                         Paddle.width, Paddle.height, 
+                         sx, sy, Paddle.colour)
+        self.vx = 0
+
+    def update(self):
+        for key in Paddle.keys:
+            if pyxel.btnp(key):
+                self.vx = Paddle.keys[key]
+            if pyxel.btnr(key):
+                self.vx = 0
+        self.x += self.vx
+
+        if self.left <= 0:
+            self.x = self.width // 2 + Paddle.margin
+        elif self.right >= self.sx:
+            self.x = self.sx - self.width // 2 - Paddle.margin
